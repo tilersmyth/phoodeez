e23964db0a5ff9ded1ed8fc3b3a2d79ef4ef61c8
Tyler Smith <tyler.smith.la@gmail.com>
2015-02-26 16:48:43 -0500

    cart dropdown stuff. WIP. I repeat, work-in-process

--- js/phoodeez.js
+++ js/phoodeez.js
@@ -7,3 +7,18 @@
 // $( window ).resize(function() {
 //     resizing($(".option-thumb"));
 // });
+
+
+//dropdown 
+$(document).ready(function() {
+	$('#cart-dropdown').on('show.bs.dropdown', function () {
+  			$( "#option_wrapper" ).addClass( "col-sm-8" );
+  			$( "#cart_dropdown_buffer" ).addClass( "col-sm-4" );
+	})
+
+	$('#cart-dropdown').on('hide.bs.dropdown', function () {
+  			$( "#option_wrapper" ).removeClass( "col-sm-8" );
+  			$( "#cart_dropdown_buffer" ).removeClass( "col-sm-4" );
+	})
+
+});
